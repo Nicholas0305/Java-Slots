c0064135073b619e31b7c8a81dd7b4ea1af72db1
Meagan <MeaganTruong1@yahoo.com>
2023-11-13 14:17:16 -0600

    grabbed some elements

---
+++ index.js
@@ -0,0 +1,6 @@
+// grab elements
+let slotMachineScreen = document.getElementById("slot-machine-screen");
+let reel = document.getElementsByClassName("reel");
+let reels = document.getElementsByClassName("reels")
+let startButton = document.getElementsByClassName("start-button");
+let stopButton = document.getElementsByClassName("stop-button");
