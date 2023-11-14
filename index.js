94eeee47e5c49a58acddaa036e4d5ba877047078
Nicholas <najernigin@gmail.com>
2023-11-14 11:54:24 -0600

    Shuffle

--- index.js
+++ index.js
@@ -5,27 +5,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const pointsDisplay = document.getElementById('points');
     const bankDisplay = document.getElementById('bank');
 
-// Fetch and setup slot machine
-let symbols = [];
+    // Fetch and setup slot machine
+    let symbols = [];
 
-fetch("http://127.0.0.1:3000/symbols")
-    .then(response => response.json())
-    .then(fetchedSymbols => {
-        spinReels()
-    })
-// Need a function
-function spinReels([...fetchedSymbols]) {
+    fetch("http://127.0.0.1:3000/symbols")
+        .then(response => response.json())
+        .then(fetchedSymbols => {
+            spinReels()
+        })
+    // Need a function
+    function spinReels([fetchedSymbols]) {
+        let newSymbols = [...fetchedSymbols]
+        function shuffle(newSymbols) {
+            for (let i = newSymbols.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [newSymbols[i], newSymbols[j]] = [newSymbols[j], newSymbols[i]];
+            }
 
-}
+        }
+        shuffle()
+        return newSymbols;
+    }
 })
 
-function shuffleReels(reel) {
-    for (let i = reel.children.length; i >= 0; i--) {
-        const randomIndex = Math.floor(Math.random() * i);
-        reel.appendChild(reel.children[randomIndex]);
-    }
-}
+// fetch array of objects
+// made a copy of the array via spread operator in the spinReels function
+//shuffle the array
+// return new copy of array with list of objects
+// iterate through the array, and asign image objects to the reels list
 
-// Call this function for each slot machine
-const slotMachines = document.querySelectorAll('.slotMachine');
-slotMachines.forEach(slotMachine => shuffleReels(slotMachine.querySelector('.reels')));
