f88091029d93a36eb306cc06a49452ad10bf5470
Meagan <MeaganTruong1@yahoo.com>
2023-11-15 16:00:06 -0600

    comments/trying to do point system

--- index.js
+++ index.js
@@ -106,13 +106,15 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 1000)
         }
     }
-
+// if 3 fruits are the same, log You Win
     function reactivatePlayButton(symbols) {
         if (reelImg1.src === reelImg2.src && reelImg2.src === reelImg3.src) {
             console.log('You Win')
+            // grab name of image, find symbol in array
             const matchedSymbolImg = reelImg1.src.split('/').pop()
             const matchedSymbol = symbols.find(symbol => symbol.image.includes(matchedSymbolImg))
             if (matchedSymbol) {
+                // add points when symbols match
                 playerPoints += matchedSymbol.points
                 updatePointsDisplay()
             }
